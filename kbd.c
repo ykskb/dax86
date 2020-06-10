c72fe07d7ad420cfa4ed13de16da6e07bd408146
ykskb <yoheikusakabe@gmail.com>
2020-06-10 22:41:21 +0800

    Fix Scan Code Map Moved declaration for warning.

--- kbd.c
+++ kbd.c
@@ -13,6 +13,150 @@
 
 #define KBD_DIB 0x01
 
+uint8_t scmap[128] = {
+    /* 0x00 */
+    NO,
+    NO,
+    NO,
+    NO,
+    NO,
+    NO,
+    NO,
+    NO,
+    0x0e, // backspace
+    0x0f, // tab
+    0x1c, // enter (LF)
+    NO,
+    NO,
+    NO,
+    NO,
+    NO,
+    /* 0x10 */
+    NO,
+    NO,
+    NO,
+    NO,
+    NO,
+    NO,
+    NO,
+    NO,
+    NO,
+    NO,
+    NO,
+    0x01, // esc
+    NO,
+    NO,
+    NO,
+    NO,
+    /* 0x20 */
+    0x39, // space
+    NO,   // !
+    NO,   // "
+    NO,   // #
+    NO,   // $
+    NO,   // %
+    NO,   // &
+    0x28, // '
+    NO,   // (
+    NO,   // )
+    NO,   // *
+    NO,   // +
+    0x33, // ,
+    0x0c, // -
+    0x34, // .
+    0x35, // /
+
+    /* 0x30 */
+    0x0b, // 0
+    0x02, // 1
+    0x03, // 2
+    0x04, // 3
+    0x05, // 4
+    0x06, // 5
+    0x07, // 6
+    0x08, // 7
+    0x09, // 8
+    0x0a, // 9
+    NO,   // :
+    0x27, // ;
+    NO,   // <
+    0x0d, // =
+    NO,   // >
+    NO,   // ?
+
+    /* 0x40 */
+    NO, // @
+    NO, // A
+    NO, // B
+    NO, // C
+    NO, // D
+    NO, // E
+    NO, // F
+    NO, // G
+    NO, // H
+    NO, // I
+    NO, // J
+    NO, // K
+    NO, // L
+    NO, // M
+    NO, // N
+    NO, // O
+
+    /* 0x50 */
+    NO,   // P
+    NO,   // Q
+    NO,   // R
+    NO,   // S
+    NO,   // T
+    NO,   // U
+    NO,   // V
+    NO,   // W
+    NO,   // X
+    NO,   // Y
+    NO,   // Z
+    0x1a, // [
+    0x2b, // backslash
+    0x1b, // ]
+    NO,   // ^
+    NO,   // _
+
+    /* 0x60 */
+    0x29, // `
+    0x1e, // a
+    0x30, // b
+    0x2e, // c
+    0x20, // d
+    0x12, // e
+    0x21, // f
+    0x22, // g
+    0x23, // h
+    0x17, // i
+    0x24, // j
+    0x25, // k
+    0x26, // l
+    0x32, // m
+    0x31, // n
+    0x18, // o
+
+    /* 0x70 */
+    0x19, // p
+    0x10, // q
+    0x13, // r
+    0x1f, // s
+    0x14, // t
+    0x16, // u
+    0x2f, // v
+    0x11, // w
+    0x2d, // x
+    0x15, // y
+    0x2c, // z
+    NO,   // {
+    NO,   // |
+    NO,   // }
+    NO,   // ~
+    0x0e  // delete character
+};
+
 KBD *kbd;
 
 uint8_t get_kbd_status()
